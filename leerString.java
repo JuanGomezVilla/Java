d8e0ee842ffad0edba966f503751adeacb10f304
Juan Gómez Villa <98175871+JuanGomezVilla@users.noreply.github.com>
2022-01-21 18:03:15 +0100

    Add files via upload

---
+++ leerString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; //Se importa la librería Scanner
+
+class lecturaString {
+   public static Scanner leer = new Scanner(System.in);     //Se crea una variable de tipo Scanner
+   public static void main(String[] args){
+      String palabra = leerString("Palabra: ");             //Se asigna a una variable un valor llamando a una función
+      System.out.println("Nombre: "+ palabra);              //El programa imprime el valor de la variable
+   }
+   
+   public static String leerString(String texto){           //Función para leer variables del tipo string
+      System.out.print(texto);                              //Imprime un texto por pantalla
+      return leer.nextLine();                               //Devuelve la línea escrita
+   }
+}
