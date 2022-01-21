558dbfc10070530fe60bc8fb410974b1672b9c21
Juan Gómez Villa <98175871+JuanGomezVilla@users.noreply.github.com>
2022-01-21 18:08:15 +0100

    Update and rename leerString.java to lecturaString.java

--- Aplicaciones de consola/lecturaString.java
+++ Aplicaciones de consola/lecturaString.java
@@ -4,7 +4,7 @@ class lecturaString {
    public static Scanner leer = new Scanner(System.in);     //Se crea una variable de tipo Scanner
    public static void main(String[] args){
       String palabra = leerString("Palabra: ");             //Se asigna un valor a partir de una función a una variable
-      System.out.println("Nombre: "+ palabra);              //El programa imprime el valor de la variable
+      System.out.println("Palabra: "+ palabra);             //El programa imprime el valor de la variable
    }
    
    public static String leerString(String texto){           //Función para leer variables de tipo string
